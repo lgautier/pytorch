e2536a631363dd87231de79beb4358b9ba15cdf3
Sun, Jiayi <jiayi.sun@intel.com>
2025-11-05 06:22:47 +0000

    [Quant][PT2E][X86]Support qconv_pointwise.tensor and qconv2d_pointwise.binary_tensor
    ghstack-source-id: 74c9809a6674f92840eff6de0008444baf41b4bd
    Pull-Request: https://github.com/pytorch/pytorch/pull/166608

--- torch/_inductor/mkldnn_ir.py
+++ torch/_inductor/mkldnn_ir.py
@@ -603,7 +603,7 @@ class QConvPointWisePT2E(ExternKernelAlloc):
             inputs,
             constant_args,
             None,
-            op_overload=torch.ops.onednn.qconv_pointwise.default,
+            op_overload=torch.ops.onednn.qconv_pointwise.tensor,
             cpp_kernel_name=f"aoti_torch_{self.device_type}__qconv_pointwise_tensor",
         )
 
@@ -711,7 +711,7 @@ class QConvPointWiseBinaryPT2E(ExternKernelAlloc):
             inputs,
             constant_args,
             None,
-            op_overload=torch.ops.onednn.qconv2d_pointwise.binary,
+            op_overload=torch.ops.onednn.qconv2d_pointwise.binary_tensor,
             cpp_kernel_name=(
                 f"aoti_torch_{self.device_type}__qconv2d_pointwise_binary_tensor"
             ),
