ec32aa37abede9fc988a6629c32f774c57c999ed
Laith Sakka <lsakka@meta.com>
2025-11-06 09:17:48 -0800

    harden backed_size_oblivious and broadcast_shapes
    ghstack-source-id: 65ef5e413e222217163b2ab899431616480c4ec6
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/167232

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -255,6 +255,17 @@ def _nested_int_aware_sort(
     )
 
 
+def size_hint(x: int | torch.SymInt, *, allow_none: bool = False) -> int | None:
+    """Gets a size hint for a given expression from the underlying shapes we had.
+    Does not introduce a guard, so only use this when you can guarantee that
+    your code is still valid for arbitrary shapes (such as optimization decisions)
+    """
+    if isinstance(x, int):
+        return x
+    assert isinstance(x, torch.SymInt)
+    return x.node.shape_env.size_hint(x.node.expr, allow_none=allow_none)
+
+
 # Wrapper on lru_cache that reports statistics at process end
 def lru_cache(
     maxsize: Optional[int],
